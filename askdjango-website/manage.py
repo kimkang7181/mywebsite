8e0f41dd3983e9fa047f5c3cb6fab8a3ab8c9df4
kang <kimkang7182@naver.com>
2019-01-04 17:48:50 +0900

    ㅇㄼㅈㄷㄻㅇ

--- askdjango-website/manage.py
+++ askdjango-website/manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askcompany.settings.dev')
-    try:                                                                                                                                                                                                                                                                                                                                                                                36
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "askcompany.settings.dev")
+    try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
