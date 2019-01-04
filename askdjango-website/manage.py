eed388ce7c4f8b2314889173bca5db2a40b69251
kimkang7182 <kimkang7182@naver.com>
2019-01-04 16:46:58 +0900

    qwef

--- askdjango-website/manage.py
+++ askdjango-website/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askcompany.settings.dev')
-    try:
+    try:                                                                                                                                                                                                                                                                                                                                                                                36
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
